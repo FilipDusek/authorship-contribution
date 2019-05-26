1099796be1cffec4fc1c081c593ebefb3d89e853
Filip Dušek <fdusek1@gmail.com>
2019-05-26 21:22:27 +0200

    removed unused params from main script

--- baseline.py
+++ baseline.py
@@ -4,12 +4,11 @@ from classifiers import BaselineClassifier as Classifier
 from problems import ProblemLoader
 from utils import save_answers, print_report
 
-def baseline(path, outpath, n=3, ft=5, pt=0.1):
-    start_time = time.time()
+def baseline(path, outpath):
     problems = ProblemLoader(path)
     for problem in problems.iter():
         print('Processing {}...'.format(problem.problem_name))
-        
+
         extractor = FeatureExtractor()
         extractor.fit(problem.train.X)
         train_data = extractor.transform(problem.train.X)
@@ -22,14 +21,7 @@ def baseline(path, outpath, n=3, ft=5, pt=0.1):
         print_report(problem, predictions)
         save_answers(problem, predictions, path, outpath)
 
-    print('elapsed time:', time.time() - start_time)
-
-
 if __name__ == '__main__':
-    # Parameters:
-    # path: Path to the main folder of a collection of attribution problems
-    # outpath: Path to an output folder
-    # n: n-gram order (default=3)
-    # ft: frequency threshold (default=5)
-    # pt: probability threshold for the reject option (default=0.1)
-    baseline('./data/problems', './data/answers', 3, 5, 0.1)
+    start_time = time.time()
+    baseline('./data/problems', './data/answers')
+    print('elapsed time:', time.time() - start_time)
