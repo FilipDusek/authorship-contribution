3a7a818f5b37c6c994271b8770b98415b14375c2
Filip Dušek <fdusek1@gmail.com>
2019-06-02 17:42:32 +0200

    updated pos fex to use only basic tags

--- feature_extractors/pos.py
+++ feature_extractors/pos.py
@@ -1,6 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction import DictVectorizer
-from collections import Counter
+from collections import Counter, defaultdict
+import string
+
 import nltk
 
 class POSFeatureExtractor:
@@ -13,9 +15,16 @@ class POSFeatureExtractor:
         self.n = n
         self.min_count = min_count
 
+    def _trans_tag(self, tag):
+        if tag[0] not in string.ascii_uppercase:
+            return None
+
+        return tag[0]
+
     def _get_counts(self, X):
         tokens = nltk.word_tokenize(X)
         pos_tags = [tag for word, tag in nltk.pos_tag(tokens)]
+        # pos_tags = [tag for tag in pos_tags if tag is not None]
         ngrams = [ngram for ngram in self.sliding_window(pos_tags, self.n)]
         counts = {k: v for k, v in Counter(ngrams).items()
                   if v > self.min_count}
