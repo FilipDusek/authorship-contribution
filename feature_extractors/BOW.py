b164add09a258cdf54551b4476ec36f6cdc47aff
Gustav <ghjerting@gmail.com>
2019-06-07 14:42:10 +0200

    Bag of words fex added. punct updated.

---
+++ feature_extractors/BOW.py
@@ -0,0 +1,42 @@
+import numpy as np
+import re
+from nltk.stem import WordNetLemmatizer
+
+
+class BOWFeatureExtractor:
+    
+    def __init__ (self, low_cut = 0.95, high_cut = 1):
+        self.low_cut = low_cut
+        self.high_cut = high_cut
+        self.lemmatizer = WordNetLemmatizer()
+        
+    def _clean(self, doc):
+        clean_doc = re.split(r'\W+', doc) 
+        lemmatized_doc = [self.lemmatizer.lemmatize(token.lower()) for token in clean_doc if token != ""]
+        return lemmatized_doc  
+    
+    def fit(self, X):
+        unique_words = {}
+        for doc in X:
+            for word in self._clean(doc):
+                if word not in unique_words:
+                    unique_words[word] = 1
+                else:
+                    unique_words[word] += 1
+        low_cut = int(self.low_cut*len(unique_words))
+        high_cut = int(self.high_cut*len(unique_words))
+        self.words = sorted(unique_words, key=unique_words.get, reverse=False)[low_cut:high_cut]
+        
+    def transform(self, X):
+        bows = []
+        for doc in X:
+            bow = np.zeros(len(self.words))
+            for word in self._clean(doc):
+                if word in self.words:
+                    bow[self.words.index(word)] += 1
+            max_count = max(bow)    
+            if max_count != 0:
+                for i in range(len(bow)):
+                    bow[i] = bow[i]/max_count
+            bows.append(bow)
+        return np.array(bows)
--- feature_extractors/punct.py
+++ feature_extractors/punct.py
@@ -1,24 +1,31 @@
 import collections as ct
 import numpy as np
-
+from string import punctuation
 
 class PunctFeatureExtractor:
-    def __init__(self, marks=5, weighted=True):
+    def __init__(self, marks=0, weighted=True):
         self.marks = marks
         self.weighted = weighted
 
     def fit(self, X):
-        targetdict = {mark: count for mark, count in ct.Counter(
-            X).items() if mark.isalnum() == False and mark != " "}
-        self.topmarks = sorted(targetdict, key=targetdict.get, reverse=True)[:self.marks]
-
+        punctuation_counts = {}
+        for i in X:
+            counts = ct.Counter(i)
+            new_punc = {k:v for k, v in counts.items() if k in punctuation}
+            punctuation_counts = {key: punctuation_counts.get(key, 0) + new_punc.get(key, 0) for key in set(punctuation_counts) | set(new_punc)}
+        if self.marks > 0:
+            self.topmarks = sorted(punctuation_counts, key=punctuation_counts.get, reverse=True)[:self.marks]
+        else:
+            self.topmarks = sorted(punctuation_counts, key=punctuation_counts.get, reverse=True)
     def _transform_single(self, document):
         vector_counts = np.zeros(len(self.topmarks))
         for i in range(len(self.topmarks)):
             vector_counts[i] = document.count(self.topmarks[i])
-            if self.weighted == True:
-                vector_counts[i] = vector_counts[i] / len(document)
-        return vector_counts
-
+        if self.weighted == True:
+            n_vector_counts = np.zeros(len(self.topmarks))
+            for punc in range(len(vector_counts)):
+                n_vector_counts[punc] = vector_counts[punc]/max(vector_counts)
+        return n_vector_counts
+    
     def transform(self, X):
         return np.array([self._transform_single(document) for document in X])
