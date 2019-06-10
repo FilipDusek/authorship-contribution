cf4fe39146031c135bf3f62356c88f521cf7929d
Filip Dušek <fdusek1@gmail.com>
2019-06-10 20:56:33 +0200

    removed unused pos extractor

--- feature_extractors/__init__.py
+++ feature_extractors/__init__.py
@@ -1,5 +1,4 @@
 from .baseline import BaselineFeatureExtractor
-from .pos import PosFeatureExtractor
 from .punct import PunctFeatureExtractor
 from .functionw import FunctionWFeatureExtractor
 from .base import FeatureExtractor
