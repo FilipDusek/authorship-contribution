f03785f11f35cf043e098c4675f5813e7fc1a619
Filip Dušek <fdusek1@gmail.com>
2019-06-11 21:38:36 +0200

    fixed NNClassifier

--- classifiers/NN.py
+++ classifiers/NN.py
@@ -1,20 +1,28 @@
 from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+import os
+
+# Disable tf debug information
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 class NNClassifier:
     def __init__(self):
         self.clf = keras.models.Sequential()
 
-    def fit(self, X_train, y_train):
-        self.clf.add(keras.layers.Dense(1000, input_dim=X_train.shape[1], activation = "sigmoid"))
+    def fit(self, X, y):
+        self.encoder = LabelEncoder().fit(y)
+        y_encoded = self.encoder.transform(y)
+        self.clf.add(keras.layers.Dense(1000, input_dim=X.shape[1], activation="sigmoid"))
         self.clf.add(keras.layers.Dropout(0.3, noise_shape=None, seed=1))
         self.clf.add(keras.layers.Dense(500, activation='sigmoid'))
         self.clf.add(keras.layers.Dropout(0.3, noise_shape=None, seed=2))
         self.clf.add(keras.layers.Dense(20, activation='softmax'))
-        self.clf.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        self.clf.fit(X_train, y_train, epochs = 80, batch_size = 80, verbose = 0)
-
+        self.clf.compile(loss='sparse_categorical_crossentropy',
+                         optimizer='adam', metrics=['accuracy'])
+        self.clf.fit(X, y_encoded, epochs=80, batch_size=80, verbose=0)
 
         return self
 
     def predict(self, X):
-        return self.clf.predict_classes(X)
+
+        return self.encoder.inverse_transform(self.clf.predict_classes(X))
--- classifiers/__init__.py
+++ classifiers/__init__.py
@@ -2,4 +2,5 @@ from .baseline import BaselineClassifier
 from .logisticregression import LogisticRegressionClassifier
 from .randomforest import RandomForestClassifier
 from .bayes import NaiveBayesClassifier
+from .NN import NNClassifier
 from .utils import to_clf
