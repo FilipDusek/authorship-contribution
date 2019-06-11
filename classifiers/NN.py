884b0b901a68c945b2ad9b5212ca3b6ddbb3b8b9
Gustav <ghjerting@gmail.com>
2019-06-11 15:48:36 +0200

    Neural network adjusted to reduce overfitting.

--- classifiers/NN.py
+++ classifiers/NN.py
@@ -5,15 +5,13 @@ class NNClassifier:
         self.clf = keras.models.Sequential()
 
     def fit(self, X_train, y_train):
-        self.clf.add(keras.layers.Dense(1000, input_dim=X_train.shape[1], activation = "relu"))
-        self.clf.add(keras.layers.Dropout(0.3, noise_shape=None, seed=None))
+        self.clf.add(keras.layers.Dense(1000, input_dim=X_train.shape[1], activation = "sigmoid"))
+        self.clf.add(keras.layers.Dropout(0.3, noise_shape=None, seed=1))
         self.clf.add(keras.layers.Dense(500, activation='sigmoid'))
-        self.clf.add(keras.layers.Dropout(0.3, noise_shape=None, seed=None))
-        self.clf.add(keras.layers.Dense(100, activation='sigmoid'))
-        self.clf.add(keras.layers.Dropout(0.3, noise_shape=None, seed=None))
-        self.clf.add(keras.layers.Dense(20, activation='sigmoid'))
+        self.clf.add(keras.layers.Dropout(0.3, noise_shape=None, seed=2))
+        self.clf.add(keras.layers.Dense(20, activation='softmax'))
         self.clf.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        self.clf.fit(X_train, y_train, epochs = 200, batch_size = 80, verbose = 0)
+        self.clf.fit(X_train, y_train, epochs = 80, batch_size = 80, verbose = 0)
 
 
         return self
